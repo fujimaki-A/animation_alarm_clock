950c7ebdb2644a14136b57e3ee59fe5c98fae46c
fujimaki-A <fujimaki.nissokousan@gmail.com>
2026-03-30 13:42:04 +0900

    led_color.h 作成

---
+++ code/led_color.h
@@ -0,0 +1,20 @@
+#ifndef LED_COLOR_H
+#define LED_COLOR_H
+
+String colorName1[] = {
+  "mangetsu", "yozora"
+};
+String colorName2[] = {
+  "sakura", "antique_ki", "antique_aka"
+};
+int colorRGB1[][3] = {
+  {38, 25, 5} ,
+  {4, 5, 10}
+};
+int colorRGB2[][3] = {
+  {38, 28, 32} ,
+  {35, 20, 5} ,
+  {26, 11, 14}
+};
+
+#endif //LED_COLOR_H
