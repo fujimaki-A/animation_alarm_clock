f74bf63d306710b4b17f2c77b7a9ae01e28b4f04
fujimaki-A <fujimaki.nissokousan@gmail.com>
2026-03-30 13:38:48 +0900

    animation.h 作成

---
+++ code/animation.h
@@ -0,0 +1,24 @@
+#ifndef ANIMATION_H
+#define ANIMATION_H
+
+#include "bitmap.h"
+
+const unsigned char* const circuit_commonAnime[] = {
+    animation1, animation2
+};
+
+const int commonAnime_frames = 2;
+
+const unsigned char* const circuit_rabitAnime[] = {
+    rabitAnime1, rabitAnime2, rabitAnime3
+};
+
+const int rabitAnime_frames = 3;
+
+const unsigned char* const circuit_sakuraAnime[] = {
+    sakuraAnime1, sakuraAnime2, sakuraAnime3, sakuraAnime4
+};
+
+const int sakuraAnime_frames = 4;
+
+#endif // ANIMATION_H
